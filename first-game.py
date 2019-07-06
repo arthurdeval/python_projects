a33495c3d43e48ba702be0f949a94acd7178b996
Arthur <arthur_deval@hotmail.com>
2019-07-06 19:27:44 +0100

    Create first-game python file

---
+++ first-game.py
@@ -0,0 +1,13 @@
+import arcade
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "First Game")
+
+arcade.start_render()
+
+
+arcade.finish_render()
+
+arcade.run()
